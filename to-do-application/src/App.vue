be10508dc2a35c9d2df704b586c3bcd22388f170
holabee <function.hola.b@gmail.com>
2021-06-14 15:27:21 +0900

    [LearnVueJS] favicon, font, icon 설정

--- to-do-application/src/App.vue
+++ to-do-application/src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 스타일가이드에서는 템플릿에서 케밥기법을 권고 -->
+    <TodoHeader></TodoHeader>
+    <TodoList></TodoList>
+    <TodoInput></TodoInput>
+    <TodoFooter></TodoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import TodoHeader from './components/TodoHeader.vue';
+import TodoList from './components/TodoList.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoFooter from './components/TodoFooter.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    // 컴포넌트 태그명 : 컴포넌트 내용(파스칼케이스 )
+    'TodoHeader': TodoHeader,
+    'TodoList': TodoList,
+    'TodoInput': TodoInput,
+    'TodoFooter': TodoFooter,
   }
 }
-</script>
 
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nanum Myeongjo', 'Noto Sans KR', 'Roboto', Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
