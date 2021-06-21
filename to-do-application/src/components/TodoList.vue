eab946500ec4dedb4bc8c60163ca31328cc6f5c1
holabee <function.hola.b@gmail.com>
2021-06-21 17:33:59 +0900

    [learnVueJS]  TodoList component

--- to-do-application/src/components/TodoList.vue
+++ to-do-application/src/components/TodoList.vue
@@ -1,20 +1,31 @@
 <template>
     <div>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li v-for="todoItem in todoItems" v-bind:key="todoItem"></li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
+    data: function(){
+        return{
+            todoItems: [],
+        }
+    },
     // vue lifecycle 
     // https://vuejs.org/v2/guide/instance.html
     // https://v3.vuejs.org/api/options-lifecycle-hooks.html
     created: function(){
+        if(localStorage.length > 0){
+            for(var i = 0; i < localStorage.length ; i++){
 
+                if(localStorage.key[i] !== 'loglevel:-webpack-dev-server'){
+                    // console.log(localStorage.key[i]);
+                    this.todoItems.push(localStorage.key[i]);
+                }
+            }
+        }
     }
 }
 </script>
