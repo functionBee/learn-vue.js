8cc5871e522d2509f83a5ce3b1b7f115ee7dfb48
holabee <function.hola.b@gmail.com>
2021-09-10 16:58:27 +0900

    [TodoApp] demo code

---
+++ to-do-application/src/components/demo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="root">
+      <p>{{ this.$store.getters.originalPrice }}</p>
+      <p>{{ this.$store.getters.doublePrice }}</p>
+      <p>{{ this.$store.getters.triplePrice }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        originalPrice(){
+
+        },
+        doublePrice(){
+
+        },
+        triplePrice(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
