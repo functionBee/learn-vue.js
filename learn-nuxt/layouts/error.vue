82a396f3ecf050d5d75103ce990ccd81c2c4566d
holabee <function.hola.b@gmail.com>
2021-09-30 09:57:17 +0900

    [LearnNuxt] create error page

---
+++ learn-nuxt/layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>404 Error</h1>
+      <p>page is not found</p>
+      <p>error.vue is a level of page components</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ learn-nuxt/pages/product.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>product page</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
